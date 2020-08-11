b3039331b9453a97dba28199c52f6660fb01b81e
gabepetersen <precariousplant@gmail.com>
2020-08-10 20:17:48 -0700

    add calendar/searchbar

--- mail-archiver/src/app/app.component.ts
+++ mail-archiver/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+  
 export class AppComponent {
   title = 'mail-archiver';
+  searchToggle: boolean = false;
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl()
+  });
+
+  toggleSearchBar() {
+    this.searchToggle = !this.searchToggle;
+  }
 }
